0d306059abee3e6d6b68d10fe8ee6e08bc6c89ac
Lei <ylxx@live.com>
2024-12-22 23:22:58 +0800

    description

--- onpanda/__info__.py
+++ onpanda/__info__.py
@@ -1,13 +1,13 @@
 __version__ = "0.0.1"
-__description__ = "onpanda"
+__description__ = "onPanda Python package"
 __license__ = "MIT"
 __author__ = "DIYer22"
 __author_email__ = "ylxx@live.com"
 __maintainer__ = "DIYer22"
 __maintainer_email__ = "ylxx@live.com"
 __github_username__ = "DIYer22"
-__url__ = "https://github.com/DIYer22/onpanda"
-__support__ = "https://github.com/DIYer22/onpanda/issues"
+__url__ = "https://github.com/on-panda/onpanda"
+__support__ = "https://github.com/on-panda/onpanda/issues"
 __classifiers__ = [
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Programming Language :: Python :: 3",
