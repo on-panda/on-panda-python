d3a8c35e26de02533253dae17566bba877885f13
Lei <ylxx@live.com>
2025-06-24 22:01:12 +0800

    v0.0.5 add panda_battle

--- onpanda/__info__.py
+++ onpanda/__info__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __description__ = "onPanda Python package"
 __license__ = "MIT"
 __author__ = "DIYer22"
--- onpanda/__init__.py
+++ onpanda/__init__.py
@@ -3,3 +3,4 @@
 from .__info__ import __version__, __description__
 from .onpanda import *
 from .parser import *
+from .panda_battle import build_panda_battle
