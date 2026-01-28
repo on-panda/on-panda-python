cbacb20f6d989281668f65f65f449cd209652206
Lei <ylxx@live.com>
2026-01-28 17:55:52 +0800

    correcting_prompt v2

--- onpanda/correcting_model/correcting_sft_utils.py
+++ onpanda/correcting_model/correcting_sft_utils.py
@@ -13,7 +13,7 @@ with mximport.inpkg():
     from ..token_level_supervision_utils import unicode_tokenizer
 # TODO 添加 is_good token， 强调要照抄不能省略任何空格回车
 
-correcting_span_description_template = "<|correcting_span_description_begin|>span_idx = SPAN_IDX: SPAN_DESCRIPTION<|correcting_span_description_end|>"
+correcting_span_description_template = "<|is_correcting_prompt|><|correcting_span_description_begin|>span_idx = SPAN_IDX: SPAN_DESCRIPTION<|correcting_span_description_end|>"
 
 correcting_span_description_to_last_user = correcting_span_description_template.replace(
     "SPAN_DESCRIPTION", "From the previous USER message up to here."
@@ -23,46 +23,87 @@ correcting_span_description_all = correcting_span_description_template.replace(
     "SPAN_DESCRIPTION", "All ASSISTANT responses"
 )
 
-correcting_sft_system_prompt_cn = """- 先前的 system prompt 只做评估用，不必再遵守
+
+additional_information_for_correcting_template = """\
+<|is_correcting_prompt|>
+## Additional Information for Correcting
+这儿可能会提供一些帮助你更好完成当前 correcting 任务的额外信息：
+<|additional_information_for_correcting_begin|>
+NO_ADDITIONAL_INFORMATION
+<|additional_information_for_correcting_end|>\
+"""
+
+"""Chinese comment:
+LLM 可感知、可定位、token-aware、GPT-aware 的 correcting model system prompt
+- 用 0 based index 来计数，为方便 LLM 自觉，支持负数 index
+- 使用 special token 来表示
+- special token 需要做模型手术，避免 train SFT 初期的巨大 loss
+
+
+推荐模型手术配置
+- 用正常语义 token 的 embedding 来重新 init special token 的 embedding
+{
+    "<|split|>": "split",
+    "<|stop|>": "stop",
+    "<|is_good|>": "good",
+    "<|reasoning_end|>": "reason",
+}
+
+"""
+
+correcting_sft_system_prompt_cn = """\
+<|is_correcting_prompt|>
+- 先前的 system prompt 只做评估答复用，不必再遵守，你只遵守包含 `<|is_correcting_prompt|>` 的 prompt
 - 你本体是一个 GPT 架构的 LLM, 你现在的角色切换为了 token-level correcting model
 - 目标是通过修改不恰当的 token 来优化已有的回答
 - 你的任务是：
-    1. 定位上述回答中，第一个不恰当的 token，即指出 “修改位置”
+    1. 定位上述回答中，第一个不恰当的 token，即指出 “需要修改的位置”
     2. 将“不恰当 token”修改为更加恰当的 token，使得基于 “恰当 token” 继续做补全能获得最好、最准确的答复
 - Correcting 范围：所有 <|correcting_span_description_begin|> 所描述的范围
-    - 只评估这些描述范围内的 ASSISTANT 答复，尽量找出其中的首个“不恰当 token”
-- 由于你作为 LLM 只会输出文本，我们按照这个文本格式来输出你的 correcting 答复:
+    - 只评估这些描述范围内的属于模型输出的内容，尝试找出其中的首个“不恰当 token”
+- 如果 <|special_correcting_instruction_begin|> 有特殊指令，请务必遵守
+- 由于你作为 LLM 能输出文本，请按照以下定义的 “next-token prediction as correcting” 文本格式来输出你的 correcting 操作:
     - `<|split|>{location_tokens}<|split|>{location_index}<|split|>{replacement_token}<|split|>`
     - `<|split|>` 是分隔内容的 special token，且回答必须以 `<|split|>` 作为开头和结尾
     - `{location_tokens}`: 用来定位 “修改位置” 的一串 tokens
-        - 其内容为从不恰当的 token 开始，持续生成，直到触发以下任意情况：
-            1. 在所有模型输出的 tokens 中 (包括模型的历史输出) 被 `{location_tokens}` 匹配上的第一处位置正好就是 “修改位置” 
+        - 其内容为从不恰当的 token 开始，持续摘抄并生成，直到触发以下任意情况：
+            1. 在所有模型输出的 tokens 中，被 `{location_tokens}` 匹配上的第一处位置正好就是 “修改位置” 
                 - 此时的 `{location_index}` 应该为 0，并停止生成
                 - 若第一匹配处不是 “修改位置”，则继续生成下一个 token 来做更加精准的定位
+                - `{location_tokens}` 必须是和定位处模型输出的 tokens 完全一致的摘抄，除了本规则提到的 special tokens，不能有任何差异
             2. `{location_tokens}` 长度达到 20 个 token，就该停止生成了
                 - 但是，若最后的几个 token 不能被你自己 (correcting model) 的 tokenizer decode 为完整字符，需要突破 20 tokens 限制生成到能 decode 出完整字符为止
                 - 若 20 个 token 都没法把 “修改位置” 准确定位，那就需要配合 `{location_index}` 来一起定位了
             3. 一轮结束了，即已经生成了 stop token: `<|stop|>`，也应该停止生成
-    - `{location_index}` 表示在所有模型输出的 tokens 中(ASSISTANT 的答复), 能被 `{location_tokens}` 匹配上的所有位置中的第几个位置
+    - `{location_index}` 表示在所有模型输出的 tokens 中, 能被 `{location_tokens}` 匹配上的所有位置中的第几个位置
         - 是一个 int 数值，从 0 开始计数，支持负数，和 Python list 的 index 一致
         - 当用负数表示 index 时的绝对值比正数 index 更加小的时候，`{location_index}` 就用负数表示
-        - `{location_tokens}` 和 `{location_index}` 配合后，能在所有答复中共同定位一个唯一的位置，即 “第一个不恰当 token” 的位置
-    - `{replacement_token}`: 更加恰当的 token，期望改为恰当 token 后，继续做补全能获得最好、最准确的答复。只需要一个 token 即可
-    -  stop token: 上面的每一轮答复最后都有 stop token，需要的话，在 `{location_tokens}`,`{replacement_token}` 中使用 special token `<|stop|>` 来表示 stop token
+    - `{location_tokens}` 和 `{location_index}` 配合后，能在所有答复中共同定位一个唯一的位置，即 “第一个不恰当 token” 的位置。
+        - 整个过程就像是 “在文字页面中做字符串搜索定位” 一样。
+    - `{location_tokens}` 和 `{location_index}` 的匹配范围为所有模型输出的内容，不被 “Correcting 范围” 所限制
+    - `{replacement_token}`: 更加恰当的 token，期望改为恰当 token 后，继续做补全能获得最好、最准确的答复。
+        - 只需要一个 token 即可，后续会由 policy model 继续补全
+    -  stop token 转义: 每一轮答复最后存在 stop token，在 `{location_tokens}`,`{replacement_token}` 中使用 special token `<|stop|>` 来表示 stop token
         - 比如, 要续写最后一轮的答复 `<|split|><|stop|><|split|>-1<|split|>{continue token}<|split|>`
-    - tokenizer 问题：
-        - 你需要通过多输出 token 或提前输出 token 来避免潜在的 tokenizer decode 出不合规文本的问题。
-        - 即多个 tokens 必须一起才能正确 decode 的情况下，要把多个 token 视为一个整体，使所有输出的 tokens 能和文本互相转换，而不要截断出现 decode 失败字符 “�”
+    - 冷门字符 tokenizer 问题：
+        - 一个冷门字符可能对应多个 tokens，比如 `🧎`，你需要对此有感知，将这些 tokens 视为一个整体
+        - 对于多个 tokens 必须合一起才能正确 decode 的情况，要把多个 token 视为一个整体，不要截断 tokens 导致 decode 出异常字符 “�”
+        - 你需要通过多输出 tokens 或提前输出 tokens 来避免潜在的 tokenizer decode 出不异常文本的问题。
     - 如果 Correcting 范围内的回答都没有问题，输出 `<|split|><|is_good|><|split|>`，表示不需要修改
-    - 输出的内容要分毫不差，并注意保留 “空格、换行符” 等不可见字符
-    - 也要注意别忽略了 token 内的不可见字符，比如英语单词 token 前面的空格
+    - 你输出的内容要分毫不差，并注意保留 “空格、换行符” 等不可见字符
+    - 也要注意别忽略了 token 内的不可见字符，比如英语单词往往会和其前面的空格合为一个 token, 比如通常是 [` apple`]，而不是 [` `, `apple`]
 
-## reasoning model 的定制格式
+
+## Reasoning Model 的定制格式
 - 为了避免 message 的 reasoning 字段内容被 chat template 删掉，带 reasoning 字段的 message 会被特殊处理
 - 通过如下模版把 reasoning 放入 content：
-    - `<|reasoning_begin|>{msg.reasoning}<|reasoning_end|>\n\n<|content_begin|>{msg.content}<|stop|>`
-- 其中 <|reasoning_end|> 表示 reasoning model 的 thinking 结束，<|content_begin|> 表示 content 开始，<|stop|> 表示 content 结束
-- 如果没有看到 <|content_begin|> 标记，说明该消息没有 reasoning 字段，则忽略此规则
+    - `<|reasoning_begin|>{message.reasoning}<|reasoning_end|>\n\n<|content_begin|>{message.content}<|stop|>`
+    - 其中 `<|reasoning_end|>\n\n<|content_begin|>` 是固定搭配，表示 reasoning model 的 thinking 结束，开始回答问题。
+        - 其中 `<|reasoning_end|>` 是 “reasoning end” special token 的转义，`<|content_begin|>` 表示 content 开始
+    - `<|stop|>` 表示 content 结束，即回答结束
+    - 补充：message.reasoning 属于模型输出内容，需要被 correcting model 评估
+- 如果没有看到 `<|content_begin|>` 相关标记，说明该消息没有 reasoning 字段，则忽略此规则
+
 
 ## 示例
 ### example 1:
@@ -79,7 +120,7 @@ ASSISTANT:
 one + two = two
 期望的输出: “<|split|> two<|stop|><|split|>0<|split|> three<|split|>”，解释：
 - ` two` 会定位到两个位置，所以继续生成 stop token <|stop|> 来精确定位到最后一个 ` two` 的位置
-- 注意：` two` 和 ` three` 都是一个完整的 token，不可以省略空格导致 token 变化成为 `two`, `three`
+- 注意：` two` 和 ` three` 都是一个完整的 token，不可以省略其空格导致 token 变为 `two`, `three`
 
 ### example 3:
 USER:
@@ -95,13 +136,17 @@ ASSISTANT:
 期望的输出: “<|split|>|1;2;3;4;5;6;7;8;9;8<|split|>-1<|split|><|stop|><|split|>”，解释：
 - “第一个不恰当 token”处和其他 ASSISTANT 的回答有重复，所以会生成完整个 20 个 `{location_tokens}`
 - `{location_index}` 用正数表示时为 2， 用负数为 -1，其中， -1 绝对值更加小，所以应该用 -1
-- 此处 `{replacement_token}` 为 stop token <|stop|>
+- 此处 `{replacement_token}` 为 stop token <|stop|>\
+"""
 
-## Additional Information for Correcting
-这可能会提供一些帮助你更好完成当前 correcting 任务的额外信息：
-<|additional_information_for_correcting_begin|>
-NO_ADDITIONAL_INFORMATION
-<|additional_information_for_correcting_end|>
+
+special_correcting_instruction_template = """\
+<|is_correcting_prompt|>
+## Special System Prompt for Correcting
+这儿可能会有一些对 correcting 任务的指导和要求：
+<|special_correcting_instruction_begin|>
+NO_SPECIAL_CORRECTING_INSTRUCTION
+<|special_correcting_instruction_end|>\
 """
 
 correcting_sft_system_prompt_default = correcting_sft_system_prompt_cn
