5f5a37ac194cc422600ffd00bc08b6740d6728c8
Lei <ylxx@live.com>
2026-02-20 19:46:31 +0800

    add aggregation_panda_score

--- onpanda/correcting_model/panda_score_mixin.py
+++ onpanda/correcting_model/panda_score_mixin.py
@@ -20,9 +20,9 @@ class PandaScoreMixin:
     def _mean(nums):
         return sum(nums) / len(nums)
 
-    def eval_panda_score(self, panda_json_paths):
+    def eval_panda_score(self, panda_json_path):
         """
-        Evaluate correcting quality on panda json files.
+        Evaluate correcting quality on panda json file(s).
 
         Returned summary metrics:
         - format_score: mean format reward on all samples
@@ -38,8 +38,9 @@ class PandaScoreMixin:
         """
         from onpanda import PandaTree
 
-        if isinstance(panda_json_paths, str):
-            panda_json_paths = [panda_json_paths]
+        if isinstance(panda_json_path, (list, tuple)):
+            panda_score_list = [self.eval_panda_score(path) for path in panda_json_path]
+            return self.aggregation_panda_score(panda_score_list)
 
         format_scores = []
         location_scores = []
@@ -50,72 +51,67 @@ class PandaScoreMixin:
         replacement_on_true_location_scores = []
         not_good_num = 0
         true_location_num = 0
-        score_results = {}
+        score_results = []
 
-        for json_path in panda_json_paths:
-            panda_tree = PandaTree(json_path, tokenizer=self.adapter.tokenizer)
-            far_correction_datas = panda_tree.build_far_correction_data_v1(self.adapter)
-            for far_correction_data_idx, far_correction_data in enumerate(
-                far_correction_datas
-            ):
-                messages = remove_msgs_after_last_response_role(
-                    far_correction_data[:-2]
-                )
-                gt_correction = far_correction_data[-1]["correcting"]
+        panda_tree = PandaTree(panda_json_path, tokenizer=self.adapter.tokenizer)
+        far_correction_datas = panda_tree.build_far_correction_data_v1(self.adapter)
+        for far_correction_data_idx, far_correction_data in enumerate(
+            far_correction_datas
+        ):
+            messages = remove_msgs_after_last_response_role(far_correction_data[:-2])
+            gt_correction = far_correction_data[-1]["correcting"]
 
-                correcting_result = self.correcting(messages)
-                far_text = correcting_result["far_text"]
-                reward_result = self.adapter.verifier.compute_reward(
-                    messages,
-                    far_text,
-                    gt_correction,
-                )
-                reward_with_feedback = reward_result["reward_with_feedback"]
+            correcting_result = self.correcting(messages)
+            far_text = correcting_result["far_text"]
+            reward_result = self.adapter.verifier.compute_reward(
+                messages,
+                far_text,
+                gt_correction,
+            )
+            reward_with_feedback = reward_result["reward_with_feedback"]
 
-                gt_is_good = bool(
-                    gt_correction["find_and_replace"].get("is_good")
-                    or gt_correction["messages_location"].get("is_good")
-                )
-                pred_is_good = bool(correcting_result.get("is_good"))
-                panda_score = dict(
-                    format_score=reward_with_feedback["format_reward"],
-                    location_score=reward_with_feedback["location_reward"],
-                    replacement_score=reward_with_feedback["replacement_reward"],
-                    is_good_cls_score=1.0 if gt_is_good == pred_is_good else 0.0,
-                )
-                reward_result["gt_find_and_replace"] = gt_correction["find_and_replace"]
-                reward_result["panda_score"] = panda_score
-                reward_result["info"] = dict(
-                    panda_path=json_path,
-                    far_correction_data_idx=far_correction_data_idx,
-                    onpanda=far_correction_data[0].get("onpanda"),
-                )
-                reward_result["correcting_result"] = {
-                    k: correcting_result[k] for k in ["corrector_response"]
-                }
-                reward_result["correcting_result"]["trimmed_messages"] = (
-                    slim_json_strings(messages)
-                )
-                score_results[(json_path, far_correction_data_idx)] = reward_result
+            gt_is_good = bool(
+                gt_correction["find_and_replace"].get("is_good")
+                or gt_correction["messages_location"].get("is_good")
+            )
+            pred_is_good = bool(correcting_result.get("is_good"))
+            panda_score = dict(
+                format_score=reward_with_feedback["format_reward"],
+                location_score=reward_with_feedback["location_reward"],
+                replacement_score=reward_with_feedback["replacement_reward"],
+                is_good_cls_score=1.0 if gt_is_good == pred_is_good else 0.0,
+            )
+            reward_result["gt_find_and_replace"] = gt_correction["find_and_replace"]
+            reward_result["panda_score"] = panda_score
+            reward_result["info"] = dict(
+                panda_path=panda_json_path,
+                far_correction_data_idx=far_correction_data_idx,
+                onpanda=far_correction_data[0].get("onpanda"),
+            )
+            reward_result["correcting_result"] = {
+                k: correcting_result[k] for k in ["corrector_response"]
+            }
+            reward_result["correcting_result"]["trimmed_messages"] = slim_json_strings(
+                messages
+            )
+            score_results.append(reward_result)
 
-                format_scores.append(panda_score["format_score"])
-                location_scores.append(panda_score["location_score"])
-                replacement_scores.append(panda_score["replacement_score"])
-                is_good_cls_scores.append(panda_score["is_good_cls_score"])
+            format_scores.append(panda_score["format_score"])
+            location_scores.append(panda_score["location_score"])
+            replacement_scores.append(panda_score["replacement_score"])
+            is_good_cls_scores.append(panda_score["is_good_cls_score"])
 
-                if not gt_is_good:
-                    not_good_num += 1
-                    location_on_not_good_scores.append(panda_score["location_score"])
-                    replacement_on_not_good_scores.append(
+            if not gt_is_good:
+                not_good_num += 1
+                location_on_not_good_scores.append(panda_score["location_score"])
+                replacement_on_not_good_scores.append(panda_score["replacement_score"])
+                if panda_score["location_score"] == 1.0:
+                    true_location_num += 1
+                    replacement_on_true_location_scores.append(
                         panda_score["replacement_score"]
                     )
-                    if panda_score["location_score"] == 1.0:
-                        true_location_num += 1
-                        replacement_on_true_location_scores.append(
-                            panda_score["replacement_score"]
-                        )
         # import boxx.g
-        return dict(
+        panda_score = dict(
             format_score=self._mean(format_scores),
             location_score=self._mean(location_scores),
             replacement_score=self._mean(replacement_scores),
@@ -134,14 +130,87 @@ class PandaScoreMixin:
             not_good_num=not_good_num,
             true_location_num=true_location_num,
             score_result_num=len(score_results),
-            panda_json_num=len(panda_json_paths),
+            panda_json_num=1,
             score_results=score_results,
         )
+        return panda_score
 
-    def summary_panda_scores(self, panda_scores):
+    def aggregation_panda_score(self, panda_scores):
+        score_results = []
+        score_result_num = 0
+        panda_json_num = 0
+        not_good_num = 0
+        true_location_num = 0
+
+        format_score_sum = 0.0
+        location_score_sum = 0.0
+        replacement_score_sum = 0.0
+        is_good_cls_score_sum = 0.0
+        location_on_not_good_score_sum = 0.0
+        replacement_on_not_good_score_sum = 0.0
+        replacement_on_true_location_score_sum = 0.0
+
+        for panda_score in panda_scores:
+            score_result_num += panda_score["score_result_num"]
+            panda_json_num += panda_score["panda_json_num"]
+            not_good_num += panda_score["not_good_num"]
+            true_location_num += panda_score["true_location_num"]
+
+            format_score_sum += (
+                panda_score["format_score"] * panda_score["score_result_num"]
+            )
+            location_score_sum += (
+                panda_score["location_score"] * panda_score["score_result_num"]
+            )
+            replacement_score_sum += (
+                panda_score["replacement_score"] * panda_score["score_result_num"]
+            )
+            is_good_cls_score_sum += (
+                panda_score["is_good_cls_score"] * panda_score["score_result_num"]
+            )
+            location_on_not_good_score_sum += (
+                panda_score["location_on_not_good_score"] * panda_score["not_good_num"]
+            )
+            replacement_on_not_good_score_sum += (
+                panda_score["replacement_on_not_good_score"]
+                * panda_score["not_good_num"]
+            )
+            replacement_on_true_location_score_sum += (
+                panda_score["replacement_on_true_location_score"]
+                * panda_score["true_location_num"]
+            )
+            score_results.extend(panda_score["score_results"])
+
+        panda_score = dict(
+            format_score=format_score_sum / score_result_num,
+            location_score=location_score_sum / score_result_num,
+            replacement_score=replacement_score_sum / score_result_num,
+            is_good_cls_score=is_good_cls_score_sum / score_result_num,
+            location_on_not_good_score=(
+                location_on_not_good_score_sum / not_good_num if not_good_num else 0.0
+            ),
+            replacement_on_not_good_score=(
+                replacement_on_not_good_score_sum / not_good_num
+                if not_good_num
+                else 0.0
+            ),
+            replacement_on_true_location_score=(
+                replacement_on_true_location_score_sum / true_location_num
+                if true_location_num
+                else 0.0
+            ),
+            not_good_num=not_good_num,
+            true_location_num=true_location_num,
+            score_result_num=score_result_num,
+            panda_json_num=panda_json_num,
+            score_results=score_results,
+        )
+        return panda_score
+
+    def summary_panda_score(self, panda_score):
         return "\n".join(
             [
-                f"{k}: {round(panda_scores[k],3)}"
+                f"{k}: {round(panda_score[k],3)}"
                 for k in (
                     "format_score",
                     "location_score",
@@ -176,7 +245,7 @@ if __name__ == "__main__":
 
     # correcting_model = build_test_correcting_model(tokenizer="/home/yl/audio/asset/tokenizer/step1f/")
     # test_json_paths = sorted(glob("/home/yl/audio/jili_tracelogs_bmk/step1f_tracelogs2_batch5_jili_bmk/*.panda.json"))[:13]
-    panda_scores = correcting_model.eval_panda_score(test_json_paths)
-    print(correcting_model.summary_panda_scores(panda_scores))
-    score_results = list(panda_scores["score_results"].values())
-    tree(score_results[-1])
+    panda_score = correcting_model.eval_panda_score(test_json_paths)
+    score_results = panda_score["score_results"]
+    tree(score_results, deep=3)
+    print(correcting_model.summary_panda_score(panda_score))
