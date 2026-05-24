a5b97bbe6db8a59ccebae8c0314abb0aa3801f5f
i-liumengyin <i-liumengyin@lmy-workspace.i-liumengyin.shai-core.svc.platform.shaipower.local>
2026-05-24 21:36:10 +0800

    solve parser </think>\n

--- onpanda/server/iterative_correction_api.py
+++ onpanda/server/iterative_correction_api.py
@@ -74,13 +74,28 @@ def create_onpanda_app(base_url, api_key, cli_config):
 
                 def reasoning_parser(message):
                     content = message["content"]
-                    splitter = correcting_config.get(
-                        "reasoning_end_splitter", "\n</think>\n"
-                    )
-                    idx = content.rfind(splitter)
-                    if idx != -1:
-                        message["content"] = content[idx + len(splitter) :]
-                        message["reasoning"] = content[:idx]
+                    # splitter = correcting_config.get(
+                    #     "reasoning_end_splitter", "\n</think>\n"
+                    # )
+                    # idx = content.rfind(splitter)
+                    # if idx != -1:
+                    #     message["content"] = content[idx + len(splitter) :]
+                    #     message["reasoning"] = content[:idx]
+
+                    splitter = correcting_config.get("reasoning_end_splitter")
+                    splitters = [splitter] if splitter else [
+                        "\n</think>\n",
+                        "</think>\n",
+                        "\n</think>\n\n",
+                        "</think>\n\n",
+                    ]
+                    for splitter in splitters:
+                        idx = content.rfind(splitter)
+                        if idx != -1:
+                            message["content"] = content[idx + len(splitter):]
+                            message["reasoning"] = content[:idx]
+                            return message
+
                     return message
 
                 chat.setdefault("parser", reasoning_parser)
